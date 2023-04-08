eb57c3f380995c9c80295838dfc93636c9afc680
Hima Chanarithichai <ditimail@gmail.com>
2023-04-08 16:57:32 +1000

    Plot home field advantage

--- test/EDA_Group4.R
+++ test/EDA_Group4.R
@@ -17,3 +17,20 @@ glimpse(goals)
 glimpse(penalty_kicks)
 glimpse(bookings)
 glimpse(substitutions)
+
+# Home advantage?
+goals %>%
+  count(home_team) %>%
+  mutate(across(home_team, as.character)) %>%
+  mutate(perc = n/sum(n)) %>%
+  mutate(labels = scales::percent(perc)) %>%
+  
+  ggplot(aes(x="",y=n,fill=home_team)) +
+  geom_bar(stat="identity") +
+  coord_polar("y") +
+  geom_text(aes(label=labels),
+            position=position_stack(vjust=0.5)) +
+  labs(
+    title = "Number of goals scored by home vs visitor teams",
+    subtitle = "Data shows home team scored more goals than visitors"
+  )
