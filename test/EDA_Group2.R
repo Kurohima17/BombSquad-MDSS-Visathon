65050ef2dd4756421a7aefb9bc3481404c583128
Hima Chanarithichai <ditimail@gmail.com>
2023-04-08 11:49:18 +1000

    Create EDA_Group2.R
    Used to mess with Group 2 of worldcup dataset

---
+++ test/EDA_Group2.R
@@ -0,0 +1,19 @@
+# install.packages("devtools")
+# install.packages("tidyverse")
+# devtools::install_github("jfjelstul/worldcup")
+
+library(worldcup)
+library(tidyverse)
+
+# Explore the first group of worldcup dataset including:
+
+# qualified_teams
+# squads
+# manager_appointments
+# referee_appointments
+
+# glimpse overview
+glimpse(qualified_teams)
+glimpse(squads)
+glimpse(manager_appointments)
+glimpse(referee_appointments)
