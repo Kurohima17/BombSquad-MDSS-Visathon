7538fbffc697ba94d656d08092e938e592276fb9
Hima Chanarithichai <ditimail@gmail.com>
2023-04-08 11:46:13 +1000

    Create EDA_Group1.R
    Used to mess with data in group 1 of worldcup dataset

---
+++ test/EDA_Group1.R
@@ -0,0 +1,32 @@
+# install.packages("devtools")
+# install.packages("tidyverse")
+# devtools::install_github("jfjelstul/worldcup")
+
+library(worldcup)
+library(tidyverse)
+
+# Explore the first group of worldcup dataset including:
+
+# tournaments
+# confederations
+# teams
+# players 
+# managers
+# referees
+# stadium
+# matches
+# awards
+
+# glimpse overview
+glimpse(tournaments)
+glimpse(confederations)
+glimpse(teams)
+glimpse(players)
+glimpse(managers)
+glimpse(referees)
+glimpse(stadiums)
+glimpse(worldcup::matches)
+glimpse(awards)
+
+head(players)
+head(awards)
